f65ea2777d26f09838c71987975ba1d7c6ae1d1a
Stuart de Haas <stuartdehaas@gmail.com>
2017-09-30 11:53:22 -0600

    Added system_monitor folder and initialized with files

---
+++ system_monitor/README.md
@@ -0,0 +1,4 @@
+==Welcome==
+
+This directory will contain the scripts and code related to the arduino-based system monitor
+
---
+++ system_monitor/system_monitor.ino
@@ -0,0 +1,21 @@
+/*
+Thanksgiving Cabin Power System Monitor
+
+Ardunio-based system to be used to monitor power generation and use at
+the Thanksgiving Cabin.
+
+Donated by the UVic Caving Club
+Created: 2017-09-30
+Stuart de Haas
+stuartdehaas@gmail.com
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
